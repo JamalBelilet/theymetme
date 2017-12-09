8246c53ed41b8d7ae431d1094baa1c0348e648bf
alaeddine <fa_dahamni@esi.dz>
2017-12-09 10:53:17 +0100

    added chart

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Chart } from 'chart.js'; 
 /**
  * Generated class for the EventDetailPage page.
  *
@@ -15,6 +15,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventDetailPage {
 
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
+
   user: any = {
     name: 'Searious Peace',
     company: 'Be Small'
@@ -26,8 +29,45 @@ export class EventDetailPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EventDetailPage');
-  }
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      
+                 type: 'line',
+                 data: {
+                     datasets: [
+                         {
+                             label: "My First dataset",
+                             fill: false,
+                             lineTension: 0.1,
+                             backgroundColor: "rgba(75,192,192,0.4)",
+                             borderColor: "rgba(75,192,192,1)",
+                             borderCapStyle: 'butt',
+                             borderDash: [],
+                             borderDashOffset: 0.0,
+                             borderJoinStyle: 'miter',
+                             pointBorderColor: "rgba(75,192,192,1)",
+                             pointBackgroundColor: "#fff",
+                             pointBorderWidth: 1,
+                             pointHoverRadius: 5,
+                             pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                             pointHoverBorderColor: "rgba(220,220,220,1)",
+                             pointHoverBorderWidth: 2,
+                             pointRadius: 1,
+                             pointHitRadius: 10,
+                             data: [65, 59, 80, 81, 56, 55, 40],
+                             spanGaps: false,
+                         },   
+                     ]
+                 },
+                 options: {
+                  legend: {
+                     display: false
+                  },
+                  tooltips: {
+                     enabled: false
+                  }
+             }
+      
+             });  }
 
   goBack() {
     this.navCtrl.pop();
